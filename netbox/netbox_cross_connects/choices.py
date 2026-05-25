8611f1a9acd3d61c761d1a15e4dadaebb1e6912d
Pedro558 <pedroafonso109@gmail.com>
2026-05-25 13:24:39 +0000

    Implement crossconnect model

---
+++ netbox/netbox_cross_connects/choices.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext_lazy as _
+
+from utilities.choices import ChoiceSet
+
+__all__ = ('CrossConnectStatusChoices',)
+
+
+class CrossConnectStatusChoices(ChoiceSet):
+    key = 'CrossConnect.status'
+
+    STATUS_PLANNED = 'planned'
+    STATUS_ACTIVE = 'active'
+    STATUS_OFFLINE = 'offline'
+    STATUS_DECOMMISSIONING = 'decommissioning'
+    STATUS_DECOMMISSIONED = 'decommissioned'
+
+    CHOICES = [
+        (STATUS_PLANNED, _('Planned'), 'cyan'),
+        (STATUS_ACTIVE, _('Active'), 'green'),
+        (STATUS_OFFLINE, _('Offline'), 'red'),
+        (STATUS_DECOMMISSIONING, _('Decommissioning'), 'yellow'),
+        (STATUS_DECOMMISSIONED, _('Decommissioned'), 'gray'),
+    ]
---
+++ netbox/netbox_cross_connects/models.py
@@ -0,0 +1,66 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from netbox.models import PrimaryModel
+
+from .choices import CrossConnectStatusChoices
+
+__all__ = ('CrossConnect',)
+
+
+class CrossConnect(PrimaryModel):
+    cross_connect_id = models.CharField(
+        verbose_name=_('cross connect ID'),
+        max_length=100,
+        unique=True,
+        validators=(
+            RegexValidator(
+                regex=r'^ID-[A-Z0-9]+-\d{5}$',
+                message=_('Cross connect ID must match the format ID-<CODE>-<5 digits>.'),
+            ),
+        ),
+    )
+    ritm = models.CharField(
+        verbose_name=_('RITM'),
+        max_length=11,
+        validators=(
+            RegexValidator(
+                regex=r'^RITM\d{7}$',
+                message=_('RITM must match the format RITM#######.'),
+            ),
+        ),
+    )
+    status = models.CharField(
+        verbose_name=_('status'),
+        max_length=50,
+        choices=CrossConnectStatusChoices,
+        default=CrossConnectStatusChoices.STATUS_PLANNED,
+    )
+    site = models.ForeignKey(
+        to='dcim.Site',
+        on_delete=models.PROTECT,
+        related_name='cross_connects',
+    )
+    tenant = models.ForeignKey(
+        to='tenancy.Tenant',
+        on_delete=models.PROTECT,
+        related_name='cross_connects',
+    )
+    activation_date = models.DateField(
+        verbose_name=_('activation date'),
+        blank=True,
+        null=True,
+    )
+    last_known_path = models.TextField(
+        verbose_name=_('last known path'),
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ('cross_connect_id',)
+        verbose_name = _('cross connect')
+        verbose_name_plural = _('cross connects')
+
+    def __str__(self):
+        return self.cross_connect_id
